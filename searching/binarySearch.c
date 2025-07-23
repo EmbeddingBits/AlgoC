eb1fb706b3128702cec9d7b320ee34e24e25aef0
embeddingbits <tspamiitesh@gmail.com>
2025-07-23 15:30:53 +0530

    Adding binary search

---
+++ searching/binarySearch.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int target, int n) {
+      int low = 0;
+      int high = n - 1;
+
+      while ( low <= high ) {
+            int mid = low + ( high - low ) / 2;
+            if (arr[mid] == target) {
+                  return mid;
+            }
+            else if (arr[mid] < target) {
+                  low = mid + 1;
+            }
+            else {
+                  high = mid - 1;
+            }
+      }
+      return -1;
+}
+
+void bubbleSort(int arr[], int n) {
+      for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                  if (arr[j] > arr[j + 1]) {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j+1] = temp;
+                  }
+            }
+      }
+}
+
+int main() {
+      int arr[] = {12, 23, 34, 45, 56, 67, 78, 90};
+      int target = 56;
+      int n = sizeof(arr) / sizeof(arr[0]);
+
+      bubbleSort(arr, n);
+
+      int index = binarySearch(arr, target, n);
+
+      if (index != -1) {
+            printf("The element %d is at %d\n", target, index);
+      }
+      else {
+            printf("The element is not found");
+      }
+}
+
