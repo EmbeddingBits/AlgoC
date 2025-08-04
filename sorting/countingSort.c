0f7905cf6932988d4eb3b976db59afe2130db336
embeddingbits <tspamiitesh@gmail.com>
2025-08-04 21:56:19 +0530

    counting sort

---
+++ sorting/countingSort.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void countingSort(int arr[], int n, int max) {
+
+      int* count = (int*)calloc(max + 1, sizeof(int));
+      int* output = (int*)malloc(n * sizeof(int));
+
+      // Count occurrences of each element
+      for (int i = 0; i < n; i++) {
+            count[arr[i]]++;
+      }
+
+      // Modify count array to store actual position of elements
+      for (int i = 1; i <= max; i++) {
+            count[i] += count[i - 1];
+      }
+
+      // Build output array
+      for (int i = n - 1; i >= 0; i--) {
+            output[count[arr[i]] - 1] = arr[i];
+            count[arr[i]]--;
+      }
+
+      // Copy sorted elements back to original array
+      for (int i = 0; i < n; i++) {
+            arr[i] = output[i];
+      }
+
+      free(count);
+      free(output);
+}
+
+// Function to find maximum element in array
+int findMax(int arr[], int n) {
+      int max = arr[0];
+      for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                  max = arr[i];
+            }
+      }
+      return max;
+}
+
+int main() {
+      int arr[] = {4, 2, 2, 8, 3, 3, 1};
+      int n = sizeof(arr) / sizeof(arr[0]);
+
+      int max = findMax(arr, n);
+      countingSort(arr, n, max);
+
+      for (int i = 0; i < n; i++) {
+            printf("%d ", arr[i]);
+      }
+
+      return 0;
+}
