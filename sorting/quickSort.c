f2feb7918a236940e71bfc227f7a8ec6fab1ae0b
embeddingbits <tspamiitesh@gmail.com>
2025-08-04 21:53:11 +0530

    quick sort

---
+++ sorting/quickSort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+// Function to swap two elements
+void swap(int* a, int* b) {
+      int temp = *a;
+      *a = *b;
+      *b = temp;
+}
+
+int partition(int arr[], int low, int high) {
+      int pivot = arr[high];    // Choose the last element as pivot
+      int i = (low - 1);        // Index of smaller element
+
+      for (int j = low; j <= high - 1; j++) {
+            // If current element is smaller than or equal to pivot
+            if (arr[j] <= pivot) {
+                  i++;    // Increment index of smaller element
+                  swap(&arr[i], &arr[j]);
+            }
+      }
+      swap(&arr[i + 1], &arr[high]);
+      return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high) {
+      if (low < high) {
+            // pi is partitioning index, arr[pi] is now at right place
+            int pi = partition(arr, low, high);
+
+            // Separately sort elements before partition and after partition
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+      }
+}
+
+int main() {
+      int arr[] = {10, 7, 8, 9, 1, 5};
+      int n = sizeof(arr) / sizeof(arr[0]);
+
+      quickSort(arr, 0, n - 1);
+
+      for (int i = 0; i < n; i++)
+            printf("%d ", arr[i]);
+      printf("\n");
+
+      return 0;
+}
